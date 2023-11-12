1422d5bdd4d3a3fa1bf63035ca3a257abb76dde6
jayasimhag1reddy <jayasimhag1reddy@gmail.com>
2023-11-11 21:52:42 -0600

    day 23 of 75 days challenge

---
+++ Leetcode/src/day23/Problem2390.java
@@ -0,0 +1,22 @@
+package day23;
+//https://leetcode.com/problems/removing-stars-from-a-string/
+public class Problem2390 {
+    public String removeStars(String s) {
+        //Stack<Character> st=new Stack<>();
+        StringBuilder sb=new StringBuilder();
+        int i=0;
+        int j=0;
+        while(i<s.length()){
+            if(s.charAt(i)=='*'){
+                sb.deleteCharAt(j-1);
+                j--;
+            }
+            else{
+                sb.append(s.charAt(i));
+                j++;
+            }
+            i++;
+        }
+        return sb.toString();
+    }
+}
