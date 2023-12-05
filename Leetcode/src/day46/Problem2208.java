a22335488ce10bb871e895bc4c68a0c92b903f69
jayasimhag1reddy <jayasimhag1reddy@gmail.com>
2023-12-05 13:06:47 -0600

    Day 46 of 75 days challenge

--- Leetcode/src/day46/Problem2208.java
+++ Leetcode/src/day46/Problem2208.java
@@ -1,2 +1,24 @@
-package day46;public class Problem2208 {
+package day46;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+//https://leetcode.com/problems/minimum-operations-to-halve-array-sum/
+public class Problem2208 {
+    public int halveArray(int[] nums) {
+        PriorityQueue<Double> pq=new PriorityQueue<Double>(Comparator.reverseOrder());
+        double sum=0;
+        for(int i:nums){
+            pq.add((double)i);
+            sum+=i;
+        }
+        double half=sum;
+        int ans=0;
+        while(half>sum/2){
+            ans++;
+            double a=pq.poll()/2;
+            half-=a;
+            pq.add(a);
+        }
+        return ans;
+    }
 }
--- Leetcode/src/day46/Problem2414.java
+++ Leetcode/src/day46/Problem2414.java
@@ -1,2 +1,18 @@
-package day46;public class Problem2414 {
+package day46;
+//https://leetcode.com/problems/length-of-the-longest-alphabetical-continuous-substring/
+public class Problem2414 {
+    public int longestContinuousSubstring(String s) {
+        int ans=0;
+        int c=1;
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i)-s.charAt(i-1)==1){
+                c++;
+            }
+            else{
+                ans=Math.max(ans,c);
+                c=1;
+            }
+        }
+        return Math.max(ans,c);
+    }
 }
